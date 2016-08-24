d61afa2fa762a71b238068517c1837c545f186ef
Meghna Malhotra <mm3886@cs.drexel.edu>
2016-08-24 07:52:32 +0200

    Updated schema.sql

--- submission/schema.sql
+++ submission/schema.sql
@@ -21,66 +21,60 @@ create table Users (
 );
 
 create table have_BodyStats (
-   stat_id integer not null,
+   stat_id integer primary key,
    user_id integer not null,
    height numeric not null, 
    weight numeric not null,
    date_x date not null, 
-   primary key (stat_id, user_id),
    foreign key (user_id) references Users(user_id) on delete cascade
 );
 
 create table walk_Steps(
-   step_id integer not null,
+   step_id integer primary key,
    user_id integer not null,
    date_x date not null,
    num_steps integer not null,
    calories_burned numeric not null,
-   primary key (step_id, user_id),
    foreign key (user_id) references Users(user_id) on delete cascade
 );
 
 create table perform_Activities (
-   activity_id integer not null,
+   activity_id integer primary key,
    user_id integer not null,
    name varchar(128) not null,
    calories_burned numeric not null,
    date_x date not null,
    start_time time not null,
    end_time time not null,
-   primary key (activity_id, user_id),
    foreign key (user_id) references Users(user_id) on delete cascade
 );
 
 create table need_Nutrition (
-   meal_id integer not null,
+   meal_id integer primary key,
    user_id integer not null,
    food_name varchar(128) not null,
    meal_type varchar(64) not null,
    calories numeric not null,
    date_x date not null,
-   primary key (meal_id, user_id),
    foreign key (user_id) references Users(user_id) on delete cascade
 );
 
 create table need_Sleep (
-   sleep_session_id integer not null,
+   sleep_session_id primary key,
    user_id integer not null,
    date_x date not null,
    start_time time not null,
    end_time time not null,
-   primary key (sleep_session_id, user_id),
    foreign key (user_id) references Users(user_id) on delete cascade
 );
 
 create table have_HeartRate (
-   hr_id integer not null,
+   hr_id integer primary key,
    user_id integer not null,
    heart_rate integer not null,
    date_x date not null,
    start_time time not null,
    end_time time not null,
-   primary key (hr_id, user_id),
    foreign key (user_id) references Users(user_id) on delete cascade
 );
 
